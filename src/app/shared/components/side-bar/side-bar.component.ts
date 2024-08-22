d0ea80504bb8314b8291564459a9a13df2fd34a9
Ricardo A. Sanjur G <ricardosanjurg@gmail.com>
2024-08-21 20:55:39 -0500

    hicimos que se desplegaran las opciones del sidebar con iconos utilizando ngFor

---
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -0,0 +1,67 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-bar',
+  templateUrl: './side-bar.component.html',
+  styleUrl: './side-bar.component.css',
+})
+export class SideBarComponent {
+  mainMenu: {
+    defaultOptions: Array<any>;
+    accessLink: Array<any>;
+  } = { defaultOptions: [], accessLink: [] };
+
+  customOptions: Array<any> = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.mainMenu.defaultOptions = [
+      {
+        name: 'Home',
+        icon: 'uil uil-home',
+        router: ['/'],
+      },
+      {
+        name: 'Buscar',
+        icon: 'uil uil-search',
+        router: ['/', 'history'],
+      },
+      {
+        name: 'Tu biblioteca',
+        icon: 'uil uil-chart',
+        router: ['/', 'favorites'],
+      },
+    ];
+
+    this.mainMenu.accessLink = [
+      {
+        name: 'Crear lista',
+        icon: 'uil uil-plus-square',
+      },
+      {
+        name: 'Canciones que te gustan',
+        icon: 'uil uil-heart-medical',
+      },
+    ];
+
+    this.customOptions = [
+      {
+        name: 'Mi lista 1',
+        router: ['/']
+      },
+      {
+        name: 'Mi lista 2',
+        router: ['/']
+      },
+      {
+        name: 'Mi lista 3',
+        router: ['/']
+      },
+      {
+        name: 'Mi lista 4',
+        router: ['/']
+      },
+    ]
+  }
+}
