1ece2fcf1544bcd06455934e2c4e0c4051734275
Ricardo A. Sanjur G <ricardosanjurg@gmail.com>
2024-08-23 17:07:25 -0500

    aprendimos e implementamos el uso del router outlet

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-media-player',
   templateUrl: './media-player.component.html',
-  styleUrl: './media-player.component.css'
+  styleUrl: './media-player.component.css',
 })
 export class MediaPlayerComponent {
-
+  mockCover: any = {
+    cover: 'https:i.scdn.co/image/ab67616d0000b27345ca41b0d2352242c7c9d4bc',
+    album: 'Gioli & Assia',
+    name: 'BEBE (Oficial)',
+  };
 }
