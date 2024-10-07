b9df129807a88f3b175912bfabe1af90ec75b019
Ricardo A. Sanjur G <ricardosanjurg@gmail.com>
2024-10-06 22:15:44 -0500

    Creamos una cosa que se parece mucho a los estados en react, pero los estamos usando para almacenar y validar los inputs de un formulario

--- src/app/modules/auth/pages/login-page/login-page.component.ts
+++ src/app/modules/auth/pages/login-page/login-page.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.css'
+  styleUrl: './login-page.component.css',
 })
 export class LoginPageComponent {
+  formLogin: FormGroup = new FormGroup({});
 
+  ngOnInit(): void {
+    this.formLogin = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(12),
+      ]),
+    });
+  }
+  sendLogin() {
+    const body = this.formLogin.value;
+  }
 }
